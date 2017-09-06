2719dd6b5600799f67bbfc94b1a44f3b0b3b199a
Mihai <st.mihay@gmail.com>
2017-09-06 17:36:28 +0200

    First part of Assignment 1

--- Assignment1/src/Main.hs
+++ Assignment1/src/Main.hs
@@ -3,3 +3,27 @@ module Main where
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+data Point = Point { x :: Double, y :: Double}
+
+point :: (Double, Double) -> Point
+point (x,y) = Point x y
+
+pointX :: Point -> Double
+pointX (Point x y) = x
+
+pointY :: Point -> Double
+pointY (Point x y) = y
+
+instance Eq Point where a == b = abs (pointX a - pointX b) < 0.01 && abs (pointY a - pointY b) < 0.01
+
+data Curve = Curve { list :: [Point] }
+curve :: Point -> [Point] -> Curve
+curve point list = Curve (point : list)
+
+getList :: Curve -> [Point]
+getList (Curve list) = list
+
+connect :: Curve -> Curve -> Curve
+connect c1 c2 = Curve (getList c1 ++ getList c2)
+
