98ff02288b79b9a3bd5dccd855d1eb4e392f8072
Mihai <st.mihay@gmail.com>
2017-09-08 12:16:20 +0200

    Assignment1 library implemented

--- Assignment1/src/Main.hs
+++ Assignment1/src/Main.hs
@@ -21,11 +21,11 @@ data Curve = Curve { list :: [Point] } deriving Show
 curve :: Point -> [Point] -> Curve
 curve point list = Curve (point : list)
 
-getList :: Curve -> [Point]
-getList (Curve list) = list
+toList :: Curve -> [Point]
+toList (Curve list) = list
 
 connect :: Curve -> Curve -> Curve
-connect c1 c2 = Curve (getList c1 ++ getList c2)
+connect c1 c2 = Curve (toList c1 ++ toList c2)
 
 -- Converts a number of degrees to radians
 degToRad :: Double -> Double
@@ -36,21 +36,21 @@ rotatePoint :: Double -> Point -> Point
 rotatePoint angle p = let a = degToRad angle in point (pointX p * cos a - pointY p * sin a, pointY p * cos a + pointX p * sin a)
 
 rotate :: Curve -> Double -> Curve
-rotate c1 angle = Curve (map (rotatePoint 180) $ getList c1)
+rotate c1 angle = Curve (map (rotatePoint 180) $ toList c1)
 
 translatePoint :: (Double, Double) -> Point -> Point
 translatePoint (x,y) p = point(x + pointX p, y + pointY p)
 
 getOffset :: Curve -> Point -> (Double, Double)
 getOffset c p =  (x2 - x1, y2 - y1) where
-  first = head $ getList c
+  first = head $ toList c
   x1 = pointX first
   x2 = pointX p
   y1 = pointY first
   y2 = pointY p
 
 translate :: Curve -> Point -> Curve
-translate c p = Curve (translatePoint (getOffset c p) `map` (getList c))
+translate c p = Curve (translatePoint (getOffset c p) `map` (toList c))
 
 data Line = Vertical Double | Horizontal Double
 
@@ -61,8 +61,8 @@ reflectPointH :: Double -> Point -> Point
 reflectPointH d (Point x y) = Point x (y - 2 * (y - d))
 
 reflect :: Curve -> Line -> Curve
-reflect c (Vertical d) = Curve ((reflectPointV d) `map` (getList c))
-reflect c (Horizontal d) = Curve ((reflectPointH d) `map` (getList c))
+reflect c (Vertical d) = Curve ((reflectPointV d) `map` (toList c))
+reflect c (Horizontal d) = Curve ((reflectPointH d) `map` (toList c))
 
 bbox :: Curve -> (Point, Point)
 bbox (Curve list) = (Point minX minY, Point maxX maxY) where
@@ -72,4 +72,18 @@ bbox (Curve list) = (Point minX minY, Point maxX maxY) where
   maxX = maximum xList
   minY = minimum yList
   maxY = maximum yList
+
+width :: Curve -> Double
+width c = let bb = bbox c 
+          in pointX (snd bb) - pointX (fst bb) 
+
+height :: Curve -> Double
+height c = let bb = bbox c 
+           in pointY (snd bb) - pointY (fst bb) 
+
+normalize :: Curve -> Curve
+normalize c = let lowerLeftBboxPoint = fst (bbox c)
+                  lowerLeftBboxPointX = pointX lowerLeftBboxPoint
+                  lowerLeftBboxPointY = pointY lowerLeftBboxPoint
+              in Curve (translatePoint (- lowerLeftBboxPointX, - lowerLeftBboxPointY) `map` (toList c)) 
 
