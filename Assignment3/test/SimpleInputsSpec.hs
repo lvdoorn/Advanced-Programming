b80e585462c454357ecdfa378888b0383f945ae2
Mihai <st.mihay@gmail.com>
2017-09-25 22:44:34 +0200

    Added Gen for ident test

--- Assignment3/test/SimpleInputsSpec.hs
+++ Assignment3/test/SimpleInputsSpec.hs
@@ -2,15 +2,23 @@ module SimpleInputsSpec (spec) where
 
 import Test.Hspec
 import Test.Hspec.QuickCheck
-
+import Test.QuickCheck
 import Parser.Impl
+
+import Data.Char
+
 import SubsAst
+
+newtype IdentGen = IdentGen [Char] deriving (Eq, Show)
+
+instance Arbitrary IdentGen where
+    arbitrary = fmap IdentGen (listOf $ elements (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "_"))
 
 prop_single_int :: Int -> Bool
 prop_single_int x = ((parseString (show x)) == (Right (Number x)))
 
-prop_ident_valid :: Bool
-prop_ident_valid = ((parse parseIdent "" "nEwVar_1") == (Right $ Var "nEwVar_1"))
+prop_ident_valid :: IdentGen -> Property
+prop_ident_valid (IdentGen x) = (x /= "" && isLetter (head x)) ==> ((parse parseIdent "" x) == (Right $ Var x))
 
 spec :: Spec
 spec = do
