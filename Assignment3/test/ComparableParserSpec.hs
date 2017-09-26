8561895a47aae719a64cb304a7a0e889aa51301b
Mihai <st.mihay@gmail.com>
2017-09-26 16:26:50 +0200

    Added tests for comparable parser

---
+++ Assignment3/test/ComparableParserSpec.hs
@@ -0,0 +1,39 @@
+module ComparableParserSpec (spec) where
+
+import Test.Hspec
+import Test.QuickCheck.Modifiers
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Parser.Impl
+
+import Data.Char
+
+import SubsAst
+import Control.Monad
+
+spec :: Spec
+spec = do
+  describe "comparableParser" $ do
+    it "parses 3+4, basic comp" $
+      (parse parseComparable "" "3+4") `shouldBe`
+      (Right (Call "+" [Number 3,Number 4]))
+
+    it "parses x-5+(-6), check for left associativity" $
+      (parse parseComparable "" "x-5+(-6)") `shouldBe`
+      Right (Call "+" [Call "-" [Var "x",Number 5],Number (-6)])
+
+    it "parses 9+(3-5), comp with parentheses" $
+      (parse parseComparable "" "9+(3-5)") `shouldBe`
+      Right (Call "+" [Number 9,Call "-" [Number 3,Number 5]])
+
+    it "parses ' 9 + ( x - 5 ) ', whitespaces comp" $
+      (parse parseComparable "" " 9 + ( x - 5 ) ") `shouldBe`
+      Right (Call "+" [Number 9,Call "-" [Var "x",Number 5]])
+
+    it "parses 3*4+5*x, comp with term, check precedence" $
+      (parse parseComparable "" "3*4+5*x") `shouldBe`
+      Right (Call "+" [Call "*" [Number 3,Number 4],Call "*" [Number 5,Var "x"]])
+
+    it "parses 3*(4+5), comp with term with parentheses" $
+      (parse parseComparable "" "3*(4+5)") `shouldBe`
+      Right (Call "*" [Number 3,Call "+" [Number 4,Number 5]])
--- Assignment3/test/TermParserSpec.hs
+++ Assignment3/test/TermParserSpec.hs
@@ -14,7 +14,7 @@ import Control.Monad
 spec :: Spec
 spec = do
   describe "termParser" $ do
-    it "parses 2*4, basic ter" $
+    it "parses 2*4, basic term" $
       (parse parseTerm "" "2*4") `shouldBe`
       (Right (Call "*" [Number 2,Number 4]))
 
@@ -22,4 +22,10 @@ spec = do
       (parse parseTerm "" "x%5*(-6)") `shouldBe`
       Right (Call "*" [Call "%" [Var "x",Number 5],Number (-6)])
 
+    it "parses 9*(x*5), term with parentheses" $
+      (parse parseTerm "" "9*(x*5)") `shouldBe`
+      Right (Call "*" [Number 9,Call "*" [Var "x",Number 5]])
 
+    it "parses ' 9 * ( x * 5 ) ', whitespaces term " $
+      (parse parseTerm "" " 9 * ( x * 5 ) ") `shouldBe`
+      Right (Call "*" [Number 9,Call "*" [Var "x",Number 5]])
