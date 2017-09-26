ce2418bad224bd1bd1f06a8691ecb5aed717bc61
Mihai <st.mihay@gmail.com>
2017-09-26 13:25:02 +0200

    Added ident tests for fail cases

--- Assignment3/test/SimpleInputsSpec.hs
+++ Assignment3/test/SimpleInputsSpec.hs
@@ -6,7 +6,7 @@ import Test.QuickCheck
 import Parser.Impl
 
 import Data.Char
-
+import Data.Either
 import SubsAst
 
 newtype IdentGen = IdentGen [Char] deriving (Eq, Show)
@@ -14,12 +14,25 @@ newtype IdentGen = IdentGen [Char] deriving (Eq, Show)
 instance Arbitrary IdentGen where
     arbitrary = fmap IdentGen (listOf $ elements (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "_"))
 
+newtype KeywordGen = KeywordGen [Char] deriving (Eq, Show)
+
+instance Arbitrary KeywordGen where
+    arbitrary = fmap KeywordGen (elements keywordsForTest)
+
+keywordsForTest :: [String]
+keywordsForTest = ["true", "false", "undefined", "for", "of", "if"]
+
 prop_single_int :: Int -> Bool
 prop_single_int x = ((parseString (show x)) == (Right (Number x)))
 
 prop_ident_valid :: IdentGen -> Property
 prop_ident_valid (IdentGen x) = (x /= "" && isLetter (head x)) ==> ((parse parseIdent "" x) == (Right $ Var x))
 
+prop_ident_keyword :: KeywordGen -> Bool
+prop_ident_keyword (KeywordGen x) = isLeft (parse parseIdent "" x)
+
+prop_ident_invalid :: IdentGen -> Property
+prop_ident_invalid (IdentGen x) = (x /= "" && (not $ isLetter (head x))) ==> (isLeft (parse parseIdent "" x))
 spec :: Spec
 spec = do
   describe "Single number" $ do
@@ -27,3 +40,6 @@ spec = do
 
   describe "Parse identifier" $ do
     prop "parses valid identifier" $ prop_ident_valid
+    prop "parses keyword as identifier" $ prop_ident_keyword
+    prop "parses invalid identifier" $ prop_ident_invalid
+  
