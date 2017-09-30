f841687d45d9eb4683656bd33504bc12a9741a55
Mihai <st.mihay@gmail.com>
2017-10-01 00:31:26 +0200

    Assignment4 - implemented Warm-up, Level 1 c, d

---
+++ Assignment4/src/twitbook.pl
@@ -0,0 +1,50 @@
+g1([person(kara, [barry, clark]),
+    person(bruce, [clark, oliver]),
+    person(barry, [kara, oliver]),
+    person(clark, [oliver, kara]),
+    person(oliver, [kara])]).
+
+
+likes(G, X, Y) :- getFriendList(G, X, FriendList), 
+                  elem(Y, FriendList).
+
+dislikes(G, X, Y) :- likes(G, Y, X), 
+                     getFriendList(G, X, XFriendList),
+                     isNotElemInFriendList(G, Y, XFriendList).
+
+popular(G, X) :- getFriendList(G, X, FriendList),
+                 isPopular(G, X, FriendList).
+
+isPopular(_, _, []).
+isPopular(G, X, [Friend|RestFriends]) :- likes(G, Friend, X),
+                                         isPopular(G, X, RestFriends). 
+
+outcast(G, X) :- getFriendList(G, X, FriendList),
+                 isOutcast(G, X, FriendList).
+
+
+isOutcast(_, _, []).
+isOutcast(G, X, [Friend|RestFriends]) :- dislikes(G, Friend, X),
+                                         isOutcast(G, X, RestFriends). 
+
+
+isHight(X, []).
+isHight(X, [H|T]) :- X>H, isHight(X,T).
+
+isNotElemInFriendList(_, _, []). 
+isNotElemInFriendList(G, X, [Friend|RestFriends]) :- different(G, X, Friend),
+                                                     isNotElemInFriendList(G, X, RestFriends).
+
+elem(X, [X|_]).
+elem(X, [_|T]) :- elem(X, T).
+
+getFriendList([person(X, FriendList)|_], X, FriendList).
+getFriendList([_|T], X, FriendList) :- getFriendList(T, X, FriendList).
+
+removeElem(X, [X|T], T). 
+removeElem(X, [H|T], [H|T1]) :- removeElem(X, T, T1).
+
+%% different = Should be able to remove twice from G <=> X, Y distinct
+different(G, X, Y) :- removeElem(person(X, _), G, NewG),
+                      removeElem(person(Y, _), NewG, _).
+
