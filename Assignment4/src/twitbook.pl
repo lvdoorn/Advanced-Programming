52cd96b57f7ba14c2504af94889fad90e72c3caf
lvdoorn <xbt504@alumni.ku.dk>
2017-10-02 14:52:57 +0200

    Remove unnecessary duplicates from admires

--- Assignment4/src/twitbook.pl
+++ Assignment4/src/twitbook.pl
@@ -1,9 +1,10 @@
 likes(G, X, Y) :- getFriendList(G, X, FriendList),
                   elem(Y, FriendList).
 
-dislikes(G, X, Y) :- likes(G, Y, X),
-                     getFriendList(G, X, XFriendList),
-                     isNotElemInFriendList(G, Y, XFriendList).
+doesNotLike(G, X, Y) :- getFriendList(G, X, Xfriends),
+	                isNotElemInFriendList(G, Y, Xfriends).
+
+dislikes(G, X, Y) :- likes(G, Y, X), doesNotLike(G, X, Y).
 
 popular(G, X) :- getFriendList(G, X, FriendList),
                  isPopular(G, X, FriendList).
@@ -39,9 +40,25 @@ isHostile([person(Name, _)|T], X, Original) :- likes(Original, Name, X),
 isHostile([person(_, Friendlist)|T], X, Original) :- isNotElemInFriendList(Original, X, Friendlist),
 	                                             isHostile(T, X, Original).
 
-admires(G, X, Y) :- different(G, X, Y), admiresHelper(G, X, Y, [X]).
+admires(G, X, Y) :- different(G, X, Y), admiresHelper(G, X, Y, [X, Y]).
 admiresHelper(G, X, Y, _) :- likes(G, X, Y).
-admiresHelper(G, X, Y, List) :- likes(G, X, Z), isNotElemInFriendList(G, Z, List), admiresHelper(G, Z, Y, [Z|List]).
+admiresHelper(G, X, Y, List) :- doesNotLike(G, X, Y), likes(G, X, Z), isNotElemInFriendList(G, Z, List), admiresHelper(G, Z, Y, [Z|List]).
+
+indifferent(G, X, Y) :- different(G, X, Y), doesNotLike(G, X, Y), indifferentHelper(G, X, Y, G).
+indifferentHelper([person(Name, _)|T], Name, Y, Original) :- indifferentHelper(T, Name, Y, Original). % Everyone is indifferent to themselves for this predicate
+indifferentHelper([person(Name, _)|T], X, Y, Original) :- doesNotLike(Original, Name, Y), indifferentHelper(T, X, Y, Original).
+indifferentHelper([person(Name, _)|T], X, Y, Original) :- likes(Original, X, Name), indifferentHelper(Original, Name, Y, Original), indifferentHelper(T, X, Y, Original).
+
+%
+%	indifferent(G, X, Y) :- getFriendList(G, X, Friendlist),
+% indifferentHelper(G, X, Y, Friendlist).
+% indifferentHelper(G, X, Y, Friendlist) :- isNotElemInFriendList(G, Y,
+% Friendlist).
+%
+% For every member in the graph we check if:
+%     - either X does not like him and we are done
+%     - or X does like him and we need to check that he does not like Y,
+%	nor likes anyone that likes Y
 
 isNotElemInFriendList(_, _, []).
 isNotElemInFriendList(G, X, [Friend|RestFriends]) :- different(G, X, Friend),
