2d3bef7e44ce215f0da8f124ea878c3f5e407ea7
Leendert van Doorn <xbt504@alumni.ku.dk>
2017-10-08 14:15:31 +0200

    Add counter module

---
+++ Assignment5/src/counter.erl
@@ -0,0 +1,46 @@
+-module(counter).
+
+-export([start/0, increment/1, increment/2, get/1]).
+
+%%%% make a server that can keep track of a counter
+
+%%% API
+
+start() -> spawn(fun () -> loop(0) end).
+
+% Increments a counter by one.
+increment(Pid) ->
+  request_reply(Pid, inc).
+
+% Increments a counter by Amount.
+increment(Pid, Amount) ->
+ request_reply(Pid, {inc, Amount}).
+
+% Returns the value of a counter.
+get(Pid) ->
+  request_reply(Pid, get).
+
+
+%%% Internal implementation
+request_reply(Pid, Request) ->
+  Pid ! {self(), Request},
+  receive
+    {Pid, Response} -> Response
+  end.
+
+loop(State) ->
+  receive
+    {From, inc} ->
+    NewState = State + 1,
+    From ! {self(), NewState},
+    loop(NewState);
+
+    {From, {inc, Amount}} ->
+    NewState = State + Amount,
+    From ! {self(), NewState},
+    loop(NewState);
+
+    {From, get} ->
+    From ! {self(), State},
+    loop(State)
+  end.
