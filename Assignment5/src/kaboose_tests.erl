38e27e1ea3a37a09b7388e921649658243f0b19e
Leendert van Doorn <xbt504@alumni.ku.dk>
2017-10-10 12:16:54 +0200

    Add failing test for guess -- no active question

--- Assignment5/src/kaboose_tests.erl
+++ Assignment5/src/kaboose_tests.erl
@@ -213,3 +213,15 @@ guess_index_out_of_range_test() ->
   kaboose:guess(ActiveRoom, Ref, -100000),
   kaboose:guess(ActiveRoom, Ref, 123456789),
   ?assertEqual({ok, [0, 0], #{"Nickname" => 0}, #{"Nickname" => 0}, false}, kaboose:timesup(ActiveRoom)).
+
+guess_no_active_question_test() ->
+  {ok, Server} = kaboose:start(),
+  {ok, Room} = kaboose:get_a_room(Server),
+  kaboose:add_question(Room, {"q1?", ["a", {correct, "c"}]}),
+  kaboose:add_question(Room, {"q2?", [{correct, "c"}]}),
+  {ActiveRoom, _} = kaboose:play(Room),
+  {ok, Ref} = kaboose:join(ActiveRoom, "Nickname"),
+  kaboose:guess(ActiveRoom, Ref, 1),
+  ?assertEqual({ok, {"q1?", ["a", {correct, "c"}]}}, kaboose:next(ActiveRoom)),
+  kaboose:guess(ActiveRoom, Ref, 1),
+  ?assertEqual({ok, [1, 0], #{"Nickname" => 0}, #{"Nickname" => 0}, false}, kaboose:timesup(ActiveRoom)).
