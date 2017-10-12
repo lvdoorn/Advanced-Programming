36ae4e251c6f05b9530b4f53c165de5bbc4895cb
Leendert van Doorn <xbt504@alumni.ku.dk>
2017-10-12 13:43:58 +0200

    Add flamingo template

---
+++ Assignment6/src/flamingo.erl
@@ -0,0 +1,11 @@
+-module(flamingo).
+-export([ new/1
+		, route/4
+		, request/4
+		]).
+
+new(Global) -> todo.
+
+route(Flamingo, Prefixes, Action, Arg) -> todo.
+
+request(Flamingo, Request, From, Ref) -> todo.
